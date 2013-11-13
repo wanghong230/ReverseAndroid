ba942b9d80677718af5b3bb12877ccacee1743b2
Hong Wang <hong.wang@sv.cmu.edu>
2013-11-12 18:32:00 -0800

    reverse_apk.sh works.

--- reverse_apk.sh
+++ reverse_apk.sh
@@ -1,4 +1,53 @@
 #!/bin/bash 
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DARE="/dare/dare"
+JAD="/jad/jad"
 
-echo "it is not working!"
+function useage {
+  echo " "
+  echo " "
+  echo "Author:Hong Wang"
+  echo "E-mail:wanghong230@gmail.com"
+  echo "Description:The program was designed to do batch processsing which turn all the apk files in directroy to be java projects."
+  echo " "
+  echo " "
+  echo "Useage: ./reverse_apk.sh [-d <output dir>] <dex or apk file directory>"
+  echo " -d <output dir>: set output directory"
+  echo " "
+  echo " "
+  echo $DIR
+  exit
+}
+
+function dareapk {
+  $DIR$DARE -d $1 $2
+}
+
+function jadjava {
+  $DIR$JAD -o -r -sjava -d $1"/src/" $2"/retargeted/**/*.class"
+}
+
+function process_one_apk {
+  dareapk $1 $2
+  jadjava $1 $1 
+}
+
+#check number of arugmenets
+if [ -z "$1" ]; then 
+  useage
+  exit
+fi
+
+echo $1
+echo $2
+
+
+# process_one_apk $1 $2
+for i in $(find $2 -type f |grep "apk"); do
+    echo item: $i
+    fname=$(basename $i)
+    process_one_apk $1$fname $i
+done
+
+
